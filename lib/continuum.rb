d04a78b5ea08905e1f7a82ac8ce54265f4836a72
Steve Price <steveprice67@gmail.com>
2011-01-21 18:37:04 -0600

    remove unecessary bits

--- lib/continuum.rb
+++ lib/continuum.rb
@@ -26,12 +26,3 @@ class Continuum
     end
   end
 end
-
-if $0 == __FILE__
-  servers = %w(10.0.0.1 10.0.0.2 10.0.0.3 10.0.0.4 10.0.0.5 10.0.0.6)
-  continuum = Continuum.new
-  servers.each { |s| continuum.add_server(s) }
-  %w(foo bar baz test key widget).each do |w|
-    puts [w, continuum.get_server(w)] * ': '
-  end
-end
